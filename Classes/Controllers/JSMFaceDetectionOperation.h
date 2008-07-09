f2b9c3ed39184219d7ed484d3025dfc6f11e1b27
Jonathon Mah <me@JonathonMah.com>
2008-07-09 19:19:29 +0930

    Changed copyright line from Playhaus to Jonathon Mah

--- Classes/Controllers/JSMFaceDetectionOperation.h
+++ Classes/Controllers/JSMFaceDetectionOperation.h
@@ -3,7 +3,7 @@
 //  Faces
 //
 //  Created by Jonathon Mah on 2008-07-09.
-//  Copyright 2008 Playhaus. All rights reserved.
+//  Copyright 2008 Jonathon Mah. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
